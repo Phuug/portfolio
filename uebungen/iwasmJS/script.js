b7a77a7411b40a6224e788f51cdf774cd2facc47
Nguyen Xuan Phuc <nguyenxuanphuc@MacBook-Air-von-Nguyen.local>
2026-09-03 09:20:54 +0700

    delete practice projects

--- uebungen/iwasmJS/script.js
+++
@@ -1,8 +0,0 @@
-const styleBtn = document.querySelector("button");
-const styleOne = document.querySelector(".styleOne");
-const styleTwo = document.querySelector(".styleTwo");
-
-styleBtn.addEventListener("click", () => {
-    styleOne.toggleAttribute("disabled");
-    styleTwo.toggleAttribute("disabled");
-});
