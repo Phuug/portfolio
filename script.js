476639475589f3acc4808ed4560cafe4eb8d56b2
Nguyen Xuan Phuc <nguyenxuanphuc@MacBook-Air-von-Nguyen.local>
2026-09-09 21:33:05 +0700

    style projects page and add JS

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const burger = document.querySelector(".burger");
 menuBtn.addEventListener("click", () => {
     burger.classList.toggle("hidden");
 });
+
+const projects = document.querySelectorAll(".project");
+const mainProject = document.querySelector(".mainProject");
+
+projects.forEach(project => {
+    project.addEventListener("click", (event) => {
+        event.preventDefault();
+        const projectValue = project.innerHTML;
+        mainProject.innerHTML = `
+        ${projectValue}
+        `
+    });
+});
